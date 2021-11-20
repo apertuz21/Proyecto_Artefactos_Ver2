366863da5e0ed43e2895399ddbc1e3bbd173703b
pertuzdev <antonio.pertuz01@gmail.com>
2021-11-19 20:14:19 -0600

    Proyecto terminado

--- proyecto.ino
+++ proyecto.ino
@@ -1,3 +1,28 @@
+/***********************************
+
+Proyecto de Programación de Artefactos - Ciclo 02/21
+
+Presentado por:
+JOSE ENRIQUE GARCIA AREVALO 00093619
+RODRIGO ANIBAL HERNANDEZ GARCIA 00050519
+ANTONIO ALEXIS PERTUZ ARÉVALO 00267016
+
+Sistema de control de temperatura, humedad y luminosidad para cultivos con envió de datos a la nube
+Cultivo elegido: Dalia
+
+Recomendaciones para el cultivo:
+Temperatura adecuada: 18ªC a 25ºC
+Humedad adecuada: 70% a 80%
+Luminosidad: Necesita abundante luz, al menos que el sol sea muy intenso, colocar en lugares de semisobra
+Riego:
+Regar 1 o 2 veces a la semana abundantemente pero sin encharcar la maceta, si la temperatura es mayor 
+de la adecuada, regar más frecuentemente.
+
+
+***********************************/
+
+
+
 #include <DHT.h>
 #include <DHT_U.h>
 
@@ -19,21 +44,26 @@
 #define AIO_USERNAME  "apertuz"                             //your AIO username at ADAFRUIT
 #define AIO_KEY       "aio_GDIe90Sn6eh5TWSvCfvnifx1G611"                                 //your AIO Key at ADAFRUIT
 
-/******************* para declarar pines y variables *************************/
-#define ON 1
-#define OFF 2
+/******************* Declarar pines y variables *************************/
 
-
-#define LED_VERDE D1    // Pin al que LED está conectado
-
-//Sensor:
+/********** Sensor **********/
 #define DHTPIN D0
 #define DHTTYPE DHT11
-
 DHT dht(DHTPIN, DHTTYPE);
 
-int ldr_input;                                //sirve para guardar el dato leído de la LDR
-int pulsador;
+/********** LDR **********/
+#define LDR A0
+int lum; // guardaremos el valor que nos de el sensor (0 a 1023)
+float luminosidad_porcentaje; // guardaremos el valor que nos da el sensor convertido a porcentaje
+float coeficiente_porcentaje = 100.0 / 1023.0; // factor para convertir el valor del sensor a porcentaje
+
+/********** LEDS: **********/
+#define LED_TEMP_BAJA D1;
+#define LED_TEMP_ALTA D2;
+#define LED_HUM_BAJA D4;
+#define LED_HUM_ALTA D3;
+#define LED_LUM_BAJA D6;
+#define LED_LUM_ALTA D5;
 
 /************ Global State (you don't need to change this!) ******************/
 
@@ -46,25 +76,41 @@ WiFiClient client;
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_USERNAME, AIO_KEY);
 
 /****************************** Feeds for Publishing***************************************/
-// Setup a feed called 'indicador' for publishing. led verde
+// Setup a feed called 'temperatura' for publishing
 Adafruit_MQTT_Publish temperatura = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temperatura");
 
-// Setup a feed called 'indicador2' for publishing.led rojo
+// Setup a feed called 'humedad' for publishing
 Adafruit_MQTT_Publish humedad = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/humedad");
 
-//// Setup a feed called 'onoff' for subscribing to changes to the button
-//Adafruit_MQTT_Subscribe boton_led = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/boton_led", MQTT_QOS_1);
+// Setup a feed called 'luminosidad' for publishing
+Adafruit_MQTT_Publish luminosidad = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/luminosidad");
+
+// Setup a feed called 'frecuencia_riego' for publishing
+Adafruit_MQTT_Publish frecuencia_riego = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/frecuencia_riego");
 
 /*************************** Sketch Code ************************************/
 
 
 //*********************************************** SetUp *****************************/
+
 void setup() {
-  //para declarar si son entradas o salidas
+  
+  /******** Declaramos dispositivos de entrada o salida ********/
+  
+  pinMode(LDR, INPUT);
+  pinMode(LED_TEMP_BAJA, OUTPUT);
+  pinMode(LED_TEMP_ALTA, OUTPUT);
+  pinMode(LED_HUM_BAJA, OUTPUT);
+  pinMode(LED_HUM_ALTA, OUTPUT);
+  pinMode(LED_LUM_BAJA, OUTPUT);
+  pinMode(LED_LUM_ALTA, OUTPUT);
   
   Serial.begin(115200); //inicio de comunicacion serie
-  dht.begin();
+  
+  dht.begin(); // inicio de comunicación con el sensor dht11
+  
   delay(10);
+  
   //****************************************** Connect to WiFi access point.
   Serial.println(); Serial.println();
   Serial.print("Connecting to ");
@@ -78,19 +124,9 @@ void setup() {
   Serial.println();
   Serial.println("WiFi connected");
   Serial.println("IP address: "); Serial.println(WiFi.localIP());
-  //********************************************* Callback Functions
-  
-//  slider.setCallback(slidercallback);
- // bomba_riego.setCallback(bomba_riego_callback);
-  
-  // Setup MQTT subscription for feed's.
-//  mqtt.subscribe(&slider);
- // mqtt.subscribe(&bomba_riego);
- pinMode(LED_VERDE, OUTPUT);
-
 }
 
-uint32_t x=0;
+
 //***************************************************** Loop ********************************/
 void loop() {
   MQTT_connect();
@@ -99,34 +135,76 @@ void loop() {
     mqtt.disconnect();
   }
 
-  digitalWrite(LED_VERDE, HIGH);
+  //********* Capturar temperatura y humedad por medio del sensor *********/
 
-  /********* Capturar temperatura por medio del sensor *********/
-
-  // Leemos la temperatura en grados centígrados (por defecto)
-  float t = dht.readTemperature();
-  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // Leemos la temperatura en grados centígrados (por defecto)
+  float h = dht.readHumidity(); 
  
   // Comprobamos si ha habido algún error en la lectura
   if (isnan(t) || isnan(h)) {
     Serial.println("Error obteniendo los datos del sensor DHT11");
     return;
   }
-  //Imprimimos la temperatura
+  //Imprimimos la temperatura y humedad
   Serial.print("Temperatura: ");
   Serial.print(t);
-  Serial.print(" *C ");
   Serial.print("  Humedad: ");
   Serial.println(h);
 
-  //publicamos la temperatura en el gauge
-  temperatura.publish(t);
-  humedad.publish(h);
+  //*********** Publicamos la temperatura y humedad en sus gauge y gráficos respectivos ***********/
+  
+  temperatura.publish(t); // Si la temperatura es menor de 18 o mayor de 25, se encienden alertas en adafruit
+  humedad.publish(h); // Si la humedad es menor de 70 o mayor de 80, se encienden alertas en adafruit
 
-  //validamos que si la temperatura es mayor que 38 grados, se encienda la alerta
-  /*if (t > 38) {
-    alerta.publish(ON);
-  }*/
+  
+  //*********** Leemos la luminosidad del LDR y la convertimos a porcentaje ***********/
+  
+  lum = analogRead(LDR);
+  luminosidad_porcentaje = lum * coeficiente_porcentaje;
+  Serial.print("luminosidad: ");
+  Serial.print(lum);
+  Serial.print(" luminosidad_porcentaje: ");
+  Serial.println(luminosidad_porcentaje);
+
+
+  //*********** Publicamos la luminosidad (procentaje) en su gauge y gráfico ***********/
+  
+  luminosidad.publish(luminosidad_porcentaje); // Si la luminosidad es menor de 45 o mayor de 90, se encienden alertas en adafruit
+
+  
+  //*********** Dependiendo del valor de la temperatura, se encenderán leds como alertas y
+  // se publicará la frecuencia con la que se recomienda regar la Dalia ***********/
+
+  // Temperatura
+  if (t < 25) {
+    frecuencia_riego.publish("Regar 1 vez a la semana"); // Si la temp es menor a 25, se recomienda regar 1 vez a la semana
+    digitalWrite(LED_TEMP_BAJA, HIGH); // Se enciende alerta por temperatura muy baja
+    digitalWrite(LED_TEMP_ALTA, LOW);
+  } else if (t >= 25 && t <= 30) {
+    frecuencia_riego.publish("Regar 2 veces a la semana"); // Si la temp es ideal, se recomienda regar 2 veces a la semana
+  } else {
+    frecuencia_riego.publish("Regar 4 veces a la semana"); // Si la temp es mayor a 30, se recomienda regar 4 veces a la semana
+    digitalWrite(LED_TEMP_BAJA, LOW);
+    digitalWrite(LED_TEMP_ALTA, HIGH); // Se enciende alerta por temperatura muy alta (regar más abundantemente)
+  }
+
+  // Humedad
+  if (h < 70) {
+    digitalWrite(LED_HUM_BAJA, HIGH); // Se enciende alerta por humedad muy baja
+    digitalWrite(LED_HUM_ALTA, LOW);
+  } else if (h > 80) {
+    digitalWrite(LED_HUM_BAJA, LOW);
+    digitalWrite(LED_HUM_ALTA, HIGH); // Se enciende alerta por humedad muy alta
+  }
+  
+  // Luminosidad
+  if (luminosidad_porcentaje < 45) {
+    digitalWrite(LED_LUM_BAJA, HIGH); // Se enciende por muy poca luz
+    digitalWrite(LED_LUM_ALTA, LOW);
+  } else if (luminosidad_porcentaje > 90) {
+    digitalWrite(LED_LUM_BAJA, LOW);
+    digitalWrite(LED_LUM_ALTA, HIGH); // Se enciende por mucha
+  }
 }
 
 
